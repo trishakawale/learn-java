91790ffae0ddeff32160ea1d2999e50b0a66ef4c
Trishakawale <trishakawale2@gmail.com>
2026-01-27 21:26:50 +0530

    ReverseNumber

---
+++ ReverseNumber.java
@@ -0,0 +1,13 @@
+class ReverseNumber {
+    public static void main(String[] args) {
+        int num = 1234, rev = 0;
+
+        while(num != 0) {
+            rev = rev * 10 + num % 10;
+            num /= 10;
+        }
+
+        System.out.println("Reversed Number: " + rev);
+    }
+}
+
