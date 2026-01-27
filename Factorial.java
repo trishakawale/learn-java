3e1a5bbc968c44554ce2bd631cbab4e5356975cc
Trishakawale <trishakawale2@gmail.com>
2026-01-27 21:31:07 +0530

    Factorial

---
+++ Factorial.java
@@ -0,0 +1,13 @@
+class Factorial {
+    static int fact(int n) {
+        if(n == 0)
+            return 1;
+        return n * fact(n - 1);
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println("Factorial: " + fact(n));
+    }
+}
+
