b67cbc892750a21589a087650e048c8ddd1d90cf
Trishakawale <trishakawale2@gmail.com>
2026-01-27 21:28:35 +0530

    Palindrome

---
+++ Palindrome.java
@@ -0,0 +1,16 @@
+class Palindrome {
+    public static void main(String[] args) {
+        int num = 121, temp = num, rev = 0;
+
+        while(temp != 0) {
+            rev = rev * 10 + temp % 10;
+            temp /= 10;
+        }
+
+        if(num == rev)
+            System.out.println("Palindrome");
+        else
+            System.out.println("Not Palindrome");
+    }
+}
+
