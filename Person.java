7bb0cc45a90d0233f229bb69b68cc393b7252253
Trishakawale <trishakawale2@gmail.com>
2026-01-29 20:49:34 +0530

    inheritance

---
+++ Person.java
@@ -0,0 +1,20 @@
+class Person {
+
+ String name;
+
+long contact;
+
+ }
+
+class Employee extends Person{
+
+ int empID;
+
+ String designation;
+ }
+Class Customer extends Person{
+
+int customerID;
+ int invoiceNo;
+
+ }
