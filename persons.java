fbe0f52133f15139f9d400e66297f07ade08d553
Trisha Kawale <trishakawale.cse24@sbjit.edu.in>
2026-01-29 22:01:11 +0530

    persons.java

---
+++ persons.java
@@ -0,0 +1,25 @@
+class Person
+
+{
+
+String name;
+
+long contact;
+
+ }
+ class Employee extends Person
+
+{
+
+int empID;
+
+String designation;
+}
+
+Class Customer extends Person
+{
+
+ int customerID;
+
+ int invoiceNo;
+ }
