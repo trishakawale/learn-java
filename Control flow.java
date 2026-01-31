ccd37496f5a34e7c5078480dc0058ac3c461bc47
Trisha Kawale <trishakawale.cse24@sbjit.edu.in>
2026-01-31 07:08:55 +0530

    Control flow.java

---
+++ Control flow.java
@@ -0,0 +1,12 @@
+// If-Else: Making decisions
+if (age >= 18) {
+    System.out.println("You are an adult.");
+} else {
+    System.out.println("You are a minor.");
+}
+
+// For Loop: Doing something a specific number of times
+for (int i = 1; i <= 5; i++) {
+    System.out.println("Counting: " + i);
+}
+
