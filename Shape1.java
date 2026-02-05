2d44a756c449a943ce3025641b59b684b3ea5474
Trisha Kawale <trishakawale.cse24@sbjit.edu.in>
2026-02-05 12:36:35 +0530

    Add ShapeDemo class to demonstrate Rectangle usage

---
+++ Shape1.java
@@ -0,0 +1,9 @@
+class ShapeDemo {
+public static void main(String[] args) {
+Shape R1 = new Rectangle(4.6, 2.3); 
+System.out.println("Area = " + 
+R1.getarea());
+System.out.println("Perimeter = " + 
+R1.getperimeter());
+}
+}
