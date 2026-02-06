b8dacebbe2df7e4247d73dcb5b7b7946a90b352f
Trisha Kawale <trishakawale.cse24@sbjit.edu.in>
2026-02-06 07:40:09 +0530

    Implement method overloading in Addition class

---
+++ Polymorphism C.java
@@ -0,0 +1,20 @@
+class Addition {
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    int add(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    double add(double a, double b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        Addition obj = new Addition();
+        System.out.println(obj.add(10, 20));
+        System.out.println(obj.add(10, 20, 30));
+        System.out.println(obj.add(10.5, 20.5));
+    }
+}
