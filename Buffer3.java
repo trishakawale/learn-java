7f8830d25f76df65740f7ab5661bd75854ffe331
Trisha Kawale <trishakawale.cse24@sbjit.edu.in>
2026-02-14 12:21:48 +0530

    Create Buffer3.java

---
+++ Buffer3.java
@@ -0,0 +1,7 @@
+class StringBufferReverse {
+    public static void main(String args[]) {
+        StringBuffer sb = new StringBuffer("Engineering");
+        sb.reverse();
+        System.out.println("Reversed String: " + sb);
+    }
+}
