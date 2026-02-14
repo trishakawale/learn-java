7fa9dc8eca19afac7cf0e141a89ed93c8b3ee884
Trisha Kawale <trishakawale.cse24@sbjit.edu.in>
2026-02-14 12:22:21 +0530

    Create Buffer4.java

---
+++ Buffer4.java
@@ -0,0 +1,7 @@
+class StringBufferCapacity {
+    public static void main(String args[]) {
+        StringBuffer sb = new StringBuffer("Java");
+        System.out.println("Length: " + sb.length());
+        System.out.println("Capacity: " + sb.capacity());
+    }
+}
