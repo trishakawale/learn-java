a5c7932f94003d9680404c9c591d6b704b98f47e
Trisha Kawale <trishakawale.cse24@sbjit.edu.in>
2026-02-22 09:29:40 +0530

    Create Dog.java

---
+++ Dog.java
@@ -0,0 +1,11 @@
+class Animal {
+  // methods and fields
+}
+
+// use of extends keyword
+// to perform inheritance
+class Dog extends Animal {
+
+  // methods and fields of Animal
+  // methods and fields of Dog
+}
