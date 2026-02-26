994afde8bce847853616a8bac0046d3d16f6b4de
Trisha Kawale <trishakawale.cse24@sbjit.edu.in>
2026-02-26 10:57:34 +0530

    Create Anii.java

---
+++ Anii.java
@@ -0,0 +1,18 @@
+class Animal {
+    void sound() {
+        System.out.println("Animal makes a sound");
+    }
+}
+
+class Dog extends Animal {
+    void sound() {
+        System.out.println("Dog barks");
+    }
+}
+
+public class OverridingDemo {
+    public static void main(String[] args) {
+        Animal a = new Dog();   // Upcasting
+        a.sound();
+    }
+}
