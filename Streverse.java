debefebcc1ee345c5c0e208707cab69725837c87
Trisha Kawale <trishakawale.cse24@sbjit.edu.in>
2026-03-01 22:15:28 +0530

    Create Streverse.java

---
+++ Streverse.java
@@ -0,0 +1,11 @@
+class StringReverse {
+    public static void main(String[] args) {
+        String str = "Java";
+        String rev = "";
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            rev = rev + str.charAt(i);
+        }
+        System.out.println(rev);
+    }
+}
