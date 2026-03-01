d32b22e1eb2e13bc60bbf8e4a65a4431d977c90b
Trisha Kawale <trishakawale.cse24@sbjit.edu.in>
2026-03-01 22:15:59 +0530

    Create Arrey.java

---
+++ Arrey.java
@@ -0,0 +1,11 @@
+class ArraySum {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30};
+        int sum = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        System.out.println("Sum = " + sum);
+    }
+}
