f4c0432981052e97dc38a3c86ff1de98e9b1546b
Trisha Kawale <trishakawale.cse24@sbjit.edu.in>
2026-03-07 23:05:44 +0530

    Create LargesstNumber.java

---
+++ LargesstNumber.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class LargestNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a, b, c;
+
+        System.out.print("Enter three numbers: ");
+        a = sc.nextInt();
+        b = sc.nextInt();
+        c = sc.nextInt();
+
+        if(a >= b && a >= c)
+            System.out.println("Largest = " + a);
+        else if(b >= a && b >= c)
+            System.out.println("Largest = " + b);
+        else
+            System.out.println("Largest = " + c);
+    }
+}
