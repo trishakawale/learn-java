e882f66be0a604dbc9f624f08361966f4c05ccba
Trisha Kawale <trishakawale.cse24@sbjit.edu.in>
2026-03-11 21:28:46 +0530

    MultipleCatch.java

---
+++ MultipleCatch.java
@@ -0,0 +1,14 @@
+public class MultipleCatch {
+    public static void main(String[] args) {
+        try {
+            int a[] = new int[5];
+            a[10] = 50;
+        }
+        catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Array index is out of limit");
+        }
+        catch (Exception e) {
+            System.out.println("General Exception");
+        }
+    }
+}
