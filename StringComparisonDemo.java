8f97aca77b9b3700c7d32d5c9019f73bc0468022
Trisha Kawale <trishakawale.cse24@sbjit.edu.in>
2026-03-21 23:07:57 +0530

    StringComparisonDemo.java

---
+++ StringComparisonDemo.java
@@ -0,0 +1,21 @@
+public class StringComparisonDemo {
+
+public static void main(String[] args) {
+
+// Using new keyword (Heap memory)
+
+new String("deepak"); String s1
+
+String s2 new String("deepak");
+
+
+System.out.println(s1 s2); // false
+
+// Using String literals String Constant Pool)
+
+String s3 "amit";
+
+String 54 "amit";
+
+System.out.println(s3 s4); //
+}}
