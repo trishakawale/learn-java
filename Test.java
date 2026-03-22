5c9d28014bf59b25c3515647dff38057b5f82552
Trisha Kawale <trishakawale.cse24@sbjit.edu.in>
2026-03-22 07:09:04 +0530

    Test.java

---
+++ Test.java
@@ -0,0 +1,19 @@
+public class Test {
+
+ static void divide() {
+
+ int a = 10;
+
+ int b = 0;
+
+ int c = a / b; // ArithmeticException
+
+ }
+
+ public static void main(String[] args) {
+
+ divide();
+
+ }
+
+}
