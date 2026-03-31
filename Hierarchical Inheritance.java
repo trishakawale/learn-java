73888fae4b10ab366ad450ee9c916a2ac58cd4ec
Trisha Kawale <trishakawale.cse24@sbjit.edu.in>
2026-03-31 06:50:10 +0530

    Add hierarchical inheritance example with Shape, Circle, and Rectangle classes

---
+++ Hierarchical Inheritance.java
@@ -0,0 +1,17 @@
+class Shape {
+    void display() {
+        System.out.println("Shape");
+    }
+}
+
+class Circle extends Shape {
+    void area() {
+        System.out.println("Circle Area");
+    }
+}
+
+class Rectangle extends Shape {
+    void area() {
+        System.out.println("Rectangle Area");
+    }
+}
